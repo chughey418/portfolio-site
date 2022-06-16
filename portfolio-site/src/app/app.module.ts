5144e3d80d986a193532bd2c002adf88cd269aa4
Claire Hughey <clairehughey@Claires-MacBook-Pro-2.local>
2022-06-15 21:31:32 -0500

    added empty components

--- portfolio-site/src/app/app.module.ts
+++ portfolio-site/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent,
+    ProjectsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
